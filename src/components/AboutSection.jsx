51dbe78e7e97af94be1a3401b977a46ea38c28d2
utkarshbothe1411 <botheutkarsh@gmail.com>
2026-01-26 17:18:52 +0530

    update

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -138,8 +138,8 @@ export const AboutSection = () => {
               </a>
 
               <a
-  href="/Utkarsh_Bothe_Resume.pdf"
-  download="Utkarsh_Bothe_Resume.pdf"
+  href="/Utkarsh Bothe_Resume.pdf"
+  download="Utkarsh Bothe_Resume.pdf"
   className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors duration-300"
 >
   Download CV
